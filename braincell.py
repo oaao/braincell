3dde619b15fe0eebd56bb79c6d105aeacdc50285
oaao <oliver.antoniu@gmail.com>
2021-12-05 14:35:25 -0700

    ref [name] — clearer method & arg names

--- braincell.py
+++ braincell.py
@@ -31,24 +31,24 @@ class SingleNeuronNetwork:
         # i.e. given-moment confidence in synaptic weighting
         return x * (1 - x)
 
-    def training_process(self, train_inputs, train_outputs, iteration_num):
+    def train(self, training_inputs, training_outputs, num_iters):
         """
         Train the NN via trial-and-error.
         Synaptic weights are adjusted with each iteration.
         """
 
-        for i in range(iteration_num):
+        for i in range(num_iters):
             # pass train set through single-neuron NN
-            output = self.reasoning_process(train_inputs)
+            output = self.reason_about(training_inputs)
 
             # compute 'error' amount (diff between actual vs. predicted output)
-            err_rate = train_outputs - output
+            err_rate = training_outputs - output
 
             # multiply error by input, then by gradient of Sigmoid curve, so that:
             # a) less confident synaptic weights become adjusted more
             # b) zero-inputs do not cause changes to weights
             adj = dot(
-                train_inputs.T, # calling matrix.T represents a matrix orthogonally
+                training_inputs.T, # calling matrix.T represents a matrix orthogonally
                 err_rate * self.__sigmoid_deriv(output)
             )
 
@@ -59,7 +59,7 @@ class SingleNeuronNetwork:
                 f"Iteration {i}:\n"
                 f"{str(output)}")
 
-    def reasoning_process(self, inputs):
+    def reason_about(self, inputs):
         """
         Evaluate a given input based on current (trained) synaptic weights.
 
@@ -76,7 +76,7 @@ class SingleNeuronNetwork:
 
         print(
             f"\nConsidering new situation {str(test_case)} -> ?:\n"
-            f"{self.reasoning_process(array(test_case))}"
+            f"{self.reason_about(array(test_case))}"
         )
 
 
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     ).T
 
     # run the training process with the training set a given number of times
-    nn.training_process(train_set_in, train_set_out, 10000)
+    nn.train(train_set_in, train_set_out, 10000)
     print(f"\nPost-training synaptic weights:\n {nn.synaptic_weights}")
 
     # test the trained NN instance with a new input situation:
